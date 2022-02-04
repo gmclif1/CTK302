0485fda192d8805020fc6fdce00983c310711821
gmclif1 <gmclif1@ilstu.edu>
2022-02-03 22:35:59 -0600

    5

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -60,20 +60,17 @@ function draw() {
     fill("green");
     ellipse(160, 101, 10, 10);
 
-    ellipse(170, 122, 15, 15);
-
     fill("grey");
     rect(width / 3, height / 2.5, 75, 100,10);
 
-    fill("pink");
-    rect(width / 3, 250, 75, 80,10);
-
     fill("purple");
-    rect(180,340,20, 50, 10);
+    rect(180,328,20, 65, 10);
 
     fill("green");
-    rect(140,340,20, 50, 10);
+    rect(140,328,20, 65, 10);
 
+    fill("pink");
+    rect(136, 254, 70, 90,10);
 
     translate (120,180);
     rotate(45);
@@ -84,6 +81,11 @@ function draw() {
     rotate(250);
     fill ("orange");
     rect(0,80, 10, 100, 200);
+
+
+    fill("pink");
+    ellipse(-40, 25, -40, 50, 10, 40, 40);
+
 
     noStroke();
 
